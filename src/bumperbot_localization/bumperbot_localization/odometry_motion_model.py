f17293f9b876f6da5c11912156f0f7835bc37d54
Renzo Damian <renzo.damian.go@gmail.com>
2024-11-11 15:07:12 +0000

    delete build

--- src/bumperbot_localization/bumperbot_localization/odometry_motion_model.py
+++ src/bumperbot_localization/bumperbot_localization/odometry_motion_model.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+import random
+import time
+from math import sin, cos, atan2, sqrt, fabs, pi
+
 import rclpy
 from rclpy.node import Node
+from geometry_msgs.msg import Pose, PoseArray
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import PoseArray, Pose
-from tf_transformations import euler_from_quaternion
-from math import sin, cos, atan2, sqrt, fabs, pi
+from tf_transformations import quaternion_from_euler, euler_from_quaternion
 
 
 def normalize(z):
@@ -25,13 +28,12 @@ def angle_diff(a, b):
 
 
 class OdometryMotionModel(Node):
-
     def __init__(self):
         super().__init__("odometry_motion_model")
-        self.is_first_odom = True
         self.last_odom_x = 0.0
         self.last_odom_y = 0.0
         self.last_odom_theta = 0.0
+        self.is_first_odom = True
 
         self.declare_parameter("alpha1", 0.1)
         self.declare_parameter("alpha2", 0.1)
@@ -42,7 +44,7 @@ class OdometryMotionModel(Node):
         self.alpha1 = self.get_parameter("alpha1").get_parameter_value().double_value
         self.alpha2 = self.get_parameter("alpha2").get_parameter_value().double_value
         self.alpha3 = self.get_parameter("alpha3").get_parameter_value().double_value
-        self.alpha4 = self.get_parameter("alpha3").get_parameter_value().double_value
+        self.alpha4 = self.get_parameter("alpha4").get_parameter_value().double_value
         self.nr_samples = (
             self.get_parameter("nr_samples").get_parameter_value().integer_value
         )
@@ -52,56 +54,97 @@ class OdometryMotionModel(Node):
             self.samples.poses = [Pose() for _ in range(self.nr_samples)]
         else:
             self.get_logger().fatal(
-                "Invalid number of samples requested: %d", self.nr_samples
+                "Invalid Number of Samples requested: %d! Exit..", self.nr_samples
             )
             return
 
-        self.odom_sub_ = self.create_subscription(
-            Odometry, "bumperbot_controller/odom", self.odomCallback, 10
+        self.odom_sub = self.create_subscription(
+            Odometry, "bumperbot_controller/odom", self.odom_callback, 10
         )
-        self.pose_array_ = self.create_publisher(
+        self.pose_array_pub = self.create_publisher(
             PoseArray, "odometry_motion_model/samples", 10
         )
 
-    def odomCallback(self, odom: Odometry):
+    def odom_callback(self, odom):
         q = [
             odom.pose.pose.orientation.x,
             odom.pose.pose.orientation.y,
             odom.pose.pose.orientation.z,
             odom.pose.pose.orientation.w,
         ]
-
         roll, pitch, yaw = euler_from_quaternion(q)
 
         if self.is_first_odom:
+            self.samples.header.frame_id = odom.header.frame_id
             self.last_odom_x = odom.pose.pose.position.x
             self.last_odom_y = odom.pose.pose.position.y
             self.last_odom_theta = yaw
-
-            self.samples.header.frame_id = odom.header.frame_id
             self.is_first_odom = False
             return
 
+        # Odometry Increment
         odom_x_increment = odom.pose.pose.position.x - self.last_odom_x
         odom_y_increment = odom.pose.pose.position.y - self.last_odom_y
+        odom_theta_increment = angle_diff(yaw, self.last_odom_theta)
 
-        odom_theta_increment = angle_diff(yaw - self.last_odom_theta)
-
+        # Motion Model
         if sqrt(pow(odom_y_increment, 2) + pow(odom_x_increment, 2)) < 0.01:
             delta_rot1 = 0.0
         else:
             delta_rot1 = angle_diff(atan2(odom_y_increment, odom_x_increment), yaw)
-        delta_trans = sqrt(pow(odom_y_increment, 2) + pow(odom_x_increment, 2))
+
+        delta_trans = sqrt(pow(odom_x_increment, 2) + pow(odom_y_increment, 2))
         delta_rot2 = angle_diff(odom_theta_increment, delta_rot1)
+
+        # Noise Model that affects the Motion Model
+        rot1_variance = self.alpha1 * delta_rot1 + self.alpha2 * delta_trans
+        trans_variance = self.alpha3 * delta_trans + self.alpha4 * (
+            delta_rot1 + delta_rot2
+        )
+        rot2_variance = self.alpha1 * delta_rot2 + self.alpha2 * delta_trans
+
+        random.seed(int(time.time()))
+
+        for sample in self.samples.poses:
+            rot1_noise = random.gauss(0.0, rot1_variance)
+            trans_noise = random.gauss(0.0, trans_variance)
+            rot2_noise = random.gauss(0.0, rot2_variance)
+            delta_rot1_draw = angle_diff(delta_rot1, rot1_noise)
+            delta_trans_draw = delta_trans - trans_noise
+            delta_rot2_draw = angle_diff(delta_rot2, rot2_noise)
+
+            sample_q = [
+                sample.orientation.x,
+                sample.orientation.y,
+                sample.orientation.z,
+                sample.orientation.w,
+            ]
+            sample_roll, sample_pitch, sample_yaw = euler_from_quaternion(sample_q)
+            sample.position.x += delta_trans_draw * cos(sample_yaw + delta_rot1_draw)
+            sample.position.y += delta_trans_draw * sin(sample_yaw + delta_rot1_draw)
+            q = quaternion_from_euler(
+                0.0, 0.0, sample_yaw + delta_rot1_draw + delta_rot2_draw
+            )
+            (
+                sample.orientation.x,
+                sample.orientation.y,
+                sample.orientation.z,
+                sample.orientation.w,
+            ) = q
+
+        # Update for the next iteration
+        self.last_odom_x = odom.pose.pose.position.x
+        self.last_odom_y = odom.pose.pose.position.y
+        self.last_odom_theta = yaw
+
+        # Publish samples
+        self.pose_array_pub.publish(self.samples)
 
 
 def main():
     rclpy.init()
-
-    odometry_motion_model = OdometryMotionModel()
-    rclpy.spin(odometry_motion_model)
-
-    odometry_motion_model.destroy_node()
+    node = OdometryMotionModel()
+    rclpy.spin(node)
     rclpy.shutdown()
 
 
